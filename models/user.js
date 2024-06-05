a63743e809a3a9446148c4ed74ce0c69063ca5bf
vikashkumar6207 <vktd828403@gmail.com>
2024-06-05 10:28:24 +0530

    Don

--- models/user.js
+++ models/user.js
@@ -2,4 +2,34 @@ var mongoose= require("mongoose");
 
 //Write your schema here with name of schema as userSchema
 
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 25,
+        unique: [true, "User name should be Unique"],
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: [true, "Email should be Unique"],
+        min: 5,
+        max: 55,
+    },
+    pswd: {
+        type: String,
+        required: true,
+        nin: 6,
+        max: 25,
+    },
+    role: {
+        type: String,
+        required: true,
+    },
+    
+},{timestamps : true});
+
 module.exports =mongoose.model("users",userSchema);
